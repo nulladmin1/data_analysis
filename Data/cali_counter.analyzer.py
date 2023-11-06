c10ac13942b84b7b84921bf90b07a4c601d96ac5
Shrey Deogade <shrey.deogade@protonmail.com>
2023-11-06 17:37:47 -0500

    Added local files

---
+++ Data/cali_counter.analyzer.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import sys
+
+print(pd.__version__)
+
+datasets = {
+    'cali_2018':'State of California- Vehicle Fuel Type Count by ZIP Code OCTOBER 2018.csv',
+    'cali_2020':'State of California- Vehicle Fuel Type Count by ZIP Code JANUARY 2020.csv',
+    'cali_2021':'State of California- Vehicle Fuel Type Count by ZIP Code JANUARY 2021.csv',
+    'cali_2022':'State of California- Vehicle Fuel Type Count by ZIP Code JANUARY 2022.csv',
+    'cali_2023':'State of California- Vehicle Fuel Type Count by ZIP Code JANUARY 2023.csv',
+}
+
+try: 
+    argv_1 = sys.argv[1]
+except: 
+    dataset = input(f'Options for datasets:\n{datasets}\n')
+else: 
+    dataset = sys.argv[1]
+
+    
+
+df = pd.read_csv(datasets[dataset])
+
+#print(df.loc[df['Fuel'] == 'Unk'])
+print(df.groupby('Fuel').size())
---
+++ Data/data_overview_copier.analyzer.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import sys
+
+print(pd.__version__)
+
+def file_choose(dataset):
+    if dataset == 'chicago':
+        return 'City of Chicago Traffic Crashes - Crashes.csv'
+    elif dataset == 'nyc':
+        return 'New York City- NYC EV Fleet Station Network.csv'
+    elif dataset == 'cali_2018':
+        return 'State of California- Vehicle Fuel Type Count by ZIP Code OCTOBER 2018.csv'
+    elif dataset == 'cali_2020':
+        return 'State of California- Vehicle Fuel Type Count by ZIP Code JANUARY 2020.csv'
+    elif dataset == 'cali_2021':
+        return 'State of California- Vehicle Fuel Type Count by ZIP Code JANUARY 2021.csv'
+    elif dataset == 'cali_2022':
+        return 'State of California- Vehicle Fuel Type Count by ZIP Code JANUARY 2022.csv'
+    elif dataset == 'cali_2023':
+        return 'State of California- Vehicle Fuel Type Count by ZIP Code JANUARY 2023.csv'
+    elif dataset == 'connecticut':
+        return 'State of Connecticut - Electric Vehicle Charging Stations.csv'
+    elif dataset == 'washington_population':
+        return 'State of Washington - Electric Vehicle Population Data.csv'
+    elif dataset == 'washington_ev_registration':
+        return 'State of Washington - Electric Vehicle Title and Registration Activity.csv'
+    elif dataset == 'epa_trends':
+        return 'United States Environmental Protection Agency – Automotive Trends Report.csv'
+    elif dataset == 'doe_inventory':
+        return 'US Department of Energy - City and County Vehicle Inventories.xlsb'
+
+if sys.argv[1] == 'doe_inventory':
+    df = pd.read_excel(file_choose(sys.argv[1]))    
+else:
+    df = pd.read_csv(file_choose(sys.argv[1]))
+
+new_df = df.head(10)
+print(new_df)
+
+new_csv = 'Descriptions/'+sys.argv[1]+'_description.csv'
+new_df.to_csv(new_csv)
