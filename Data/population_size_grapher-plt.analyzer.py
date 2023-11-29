a39d8de97c2d40622a57c15ef42aa64924fdb856
Shrey Deogade <shrey.deogade@protonmail.com>
2023-11-28 19:06:19 -0500

    Added analysis of data from washington

---
+++ Data/population_size_grapher-plt.analyzer.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('Electric_Vehicle_Population_Size_History(data.wa.gov).csv')
+
+plt.rcParams['figure.autolayout'] = True
+
+h = df.columns.values
+
+df.set_index('Date').plot()
+
+plt.show()
